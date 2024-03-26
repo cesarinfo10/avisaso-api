1f3cb654f82760e576cd3454eabea129883c84c0
casv <cesarinfo10@gmail.com>
2024-03-26 09:16:10 -0300

    Api avisaso 0.0

--- src/modules/tipo_usuario/dto/usuarios.ts
+++ src/modules/tipo_usuario/dto/usuarios.ts
@@ -1,15 +1,14 @@
 import { IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from "class-validator";
 
-export class UsuarioDto {
+export class TipoUsuarioDto {
 
     @IsOptional()
     @IsNumber()
     @IsPositive()
     id?: number;
 
-    @IsNotEmpty()    
+    @IsNotEmpty()
     @IsNumber()
-    @IsPositive()
     tipo_usuario: number;
 
     @IsNotEmpty()
@@ -24,39 +23,17 @@ export class UsuarioDto {
     @IsString()
     dni: string;
 
-    @IsNotEmpty()
     @IsString()
     correo: string;
 
-    @IsNotEmpty()
     @IsString()
     celular: string;
 
     @IsString()
     telefono: string;
 
-    @IsString()
-    direccion: string;
-
-    @IsString()
-    foto_perfil: string;
-    
-    @IsString()
-    usuario: string;
-
-    @IsString()
-    password: string;
-
-    @IsString()
-    rep_password: string;
-
-    @IsString()
-    id_notificaciones: string;
-
-    @IsString()
-    carta_presentacion: string;
-
-    @IsString()
-    estado: string;
-
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
+    estado: number;
 }
